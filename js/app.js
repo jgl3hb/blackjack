04699e4be54d12d5ff6d84efa268b6181407a2ea
jgl3hb <lewis.jeffreyg@gmail.com>
2022-02-13 21:55:08 -0500

    input fields add

--- js/app.js
+++ js/app.js
@@ -10,14 +10,14 @@ let cardToRemove, cardPicked
 /*------------------------ Cached Element References ------------------------*/
 let deckEl = document.getElementById('deck')
 let burnpileEl = document.getElementById('burnpile')
-let bet1 = document.getElementById('bet-1')
-let bet5 = document.getElementById('bet-5')
-let bet25 = document.getElementById('bet-25')
-let bet100 = document.getElementById('bet-100')
-let deal = document.getElementById('deal')
-let reset = document.getElementById('reset')
-let hit = document.getElementById('hit')
-let stand = document.getElementById('stand')
+let bet1El = document.getElementById('bet-1')
+let bet5El = document.getElementById('bet-5')
+let bet25El = document.getElementById('bet-25')
+let bet100El = document.getElementById('bet-100')
+let dealEl = document.getElementById('deal')
+let resetEl = document.getElementById('reset')
+let hitEl = document.getElementById('hit')
+let standEl = document.getElementById('stand')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -55,19 +55,17 @@ init()
 //   deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 // } 
 
-//// DEALER SIX DECKS
+// SIX DECKS
 function init() {
   deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02","dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02","dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02","dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02","dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02","dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 }
 
+//Bet function
+bet1El
+
 
 //Shuffle function
-function shuffle(deck) {
-  for (let i = deck.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-		[deck[i], deck[j]] = [deck[j], deck[i]];
-  }
-}
+deck.sort(() => Math.random() - .5)
 console.log(deck)
 
 
