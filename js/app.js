0545cfd3a8639a9164eb76f43955c2b8cf0a32d5
jgl3hb <lewis.jeffreyg@gmail.com>
2022-02-17 23:44:38 -0500

    showtime

--- js/app.js
+++ js/app.js
@@ -1,28 +1,9 @@
-/*-------------------------------- Constants --------------------------------*/
-
-/*---------------------------- Variables (state) ----------------------------*/
 let deck = []
 let playerHand = []
 let dealerHand = []
 let burnpile = []
 let cardToRemove, cardPicked
 
-
-
-// function playerBank(bet) {
-// 	if (bet.value > bank) {
-// 		statusEL.innerText = "Not Enough $$$"
-// 		console.log(playerBet)
-// 	}
-//Bet function
-// const pElement = document.getElementById('bet-1')
-// pElement.innerText = "$1"
-// console.log
-// function bet(){
-// bank = bank - bet
-
-
-/*------------------------ Cached Element References ------------------------*/
 const statusEL = document.getElementById('status')
 const bet1Btn = document.getElementById('bet-1')
 const bet5Btn = document.getElementById('bet-5')
@@ -39,8 +20,6 @@ const dealerTotalEl = document.getElementById('dealerhandvalue')
 const playerDeckEl = document.getElementById('player-cards')
 const dealerDeckEl = document.getElementById('dealer-cards')
 
-
-/*----------------------------- Event Listeners -----------------------------*/
 bet1Btn.addEventListener('click', () => {
 	bet.innerText = "Bet $1"
   bank.innerText = `$${parseInt(bank.textContent) - parseInt(bet1Btn.value)}`
@@ -73,7 +52,6 @@ document.getElementById('hit').addEventListener('click', hit)
 document.getElementById('stand').addEventListener('click', stand)
 document.getElementById('deal').addEventListener('click', initialDeal)
 
-/*-------------------------------- Functions --------------------------------*/
 init()
 
 function init() {
@@ -81,15 +59,13 @@ function init() {
 }
 
 deck.sort(() => Math.random() - .5)
-console.log(deck)
 
 function handleClick() {
 	if (deck.length > 0) {
     let randIdx = Math.floor(Math.random() * deck.length)
     let cardPicked = deck[randIdx]
     playerHand.push(cardPicked)
-		console.log(playerHand)
-    render(cardPicked)
+		render(cardPicked)
 	}
 }	
 
@@ -121,11 +97,6 @@ function initialDeal() {
 	dealToDealer()
 	let playerScore = computeHandTotal(playerHand)
 	let dealerScore = computeHandTotal(dealerHand)
-	console.log(deck)
-	console.log(playerHand)
-	console.log(dealerHand)
-	console.log(playerScore)
-	console.log(dealerScore)
 	renderCards()
 	renderBlackjack()
 }	
@@ -187,8 +158,6 @@ function hit() {
 function checkHands() {
 	let playerScore = computeHandTotal(playerHand)
 	let dealerScore = computeHandTotal(dealerHand)
-	console.log(playerScore)
-	console.log(dealerScore)
 	if (playerScore > 21) {
 		renderPlayerBust()		
 	}
@@ -204,12 +173,8 @@ function stand(){
 		if (dealerScore > 21) {
 			renderDealerBust()
 		}
-		console.log(dealerHand)
+
 	}	
-	console.log(deck)
-	console.log(playerHand)
-	console.log(playerScore)
-	console.log(dealerScore)
 	renderCards()
 	renderBlackjack()
 	displayDealerCards()
@@ -221,22 +186,20 @@ function renderBlackjack(){
 	let playerScore = computeHandTotal(playerHand)
 	if(playerScore === 21 && dealerScore === 21){
 		statusEL.textContent = "Push"
-		console.log('Push')
 		displayDealerCards()
 		return
 	}else if(playerScore === 21 && dealerScore !== 21){
 		statusEL.textContent = "Player Blackjack!"
-		console.log('Player Wins')
 		displayDealerCards()
 		return
 	}else if(playerScore !== 21 && dealerScore === 21){ 
 		statusEL.textContent = "Dealer Blackjack"
-		console.log('Dealer wins')
 		displayDealerCards()
 		return
 		
 	}
 }
+
 function renderPlayerBust(){
 	statusEL.textContent = "Player Bust"
 }
@@ -250,11 +213,9 @@ function renderWin(){
 	let playerScore = computeHandTotal(playerHand)
 	if (playerScore > dealerScore && playerScore < 22){
 	statusEL.textContent = "Player Wins!";
-	console.log('Player wins')
 	return
 	} else if (dealerScore > playerScore && dealerScore < 22){
 	statusEL.textContent = "Dealer Wins";
-	console.log('Dealer wins')
 	return
 	}
 }
@@ -288,89 +249,3 @@ function displayDealerCards(){
 		dealerDeckEl.appendChild(newCardDiv)
 	})
 }
-
-
-
-
-// function aceCard(){
-// 	let dealerScore = computeHandTotal(dealerHand)
-// 	let playerScore = computeHandTotal(playerHand)
-// 	if (playerScore > 21) {
-
-	
-// }
-
-
-// $$$$$$$$$        ACE LOGIC                   $$$$$$$$$$$$$$
-//ACE =1
-
-
-
-// let playerScore = computeHandTotal(playerHand)
-
-
-// Function to render deck state
-// function render(cardPicked) {
-//   // console.log(cardPicked)
-//   // Remove outline class when first card is picked
-//   // playerHand.classList.remove('outline')
-//   // Removes previous picked card from deck 2 class list
-//   if (playerHand.length > 1) {
-//     deck.classList.remove(cardToRemove)
-//   }
-//   // Store card to be removed next iteration
-//   let cardToRemove = cardPicked  
-//   // Add current card (class name) picked to deck 2 element
-//   playerHandEl.classList.add(cardPicked)
-//   // Adjust shadow when deck gets above/below halfway full
-//   // Remove card back color and add outline when last card is picked
-// }
-
-// THE RULES
-
-
-
-//Game 1
-
-//Player Bank=  $500
-//Bet reset to 0
-//Place bet (1, 10, 25, 50) < Player Bank
-//Card 2 - 9 equals card value
-
-//Card 10 & Face cards = 10
-//Ace = 11, unless Player Hand > 21
-//then Ace = 1
-//Dealer deals two cards to Player face up
-//Dealer deals one card face up, one card face down to Dealer
-//if dealer AND Player has 21, PUSH
-//Else if Player has 21, Player Blackjack
-//Else if Dealer has 21, Dealer wins
-
-
-
-// If you go over 21 you bust, and the dealer wins regardless of the dealer's hand.
-// If you are dealt 21 from the start (Ace & 10), you got a blackjack.
-// Blackjack usually means you win 1.5 the amount of your bet. Depends on the casino.
-// Dealer will hit until his/her cards total 17 or higher.
-// Doubling is like a hit, only the bet is doubled and you only get one more card.
-// Split can be done when you have two of the same card - the pair is split into two hands.
-// Splitting also doubles the bet, because each new hand is worth the original bet.
-// You can only double/split on the first move, or first move of a hand created by a split.
-// You cannot play on two aces after they are split.
-// You can double on a hand resulting from a split, tripling or quadrupling you bet.
-
-//Else
-//Player hits or stands
-//If Player has 21, Player wins
-//If Player has > 21, Dealer wins
-//If Player Hand < 21
-//if stand dealer hits
-//Dealer Hits on 16, Stand's on 17
-//If Player Hand > Dealer Hand, Player wins
-//If Dealer Hand > Player Hand, Dealer wins
-//If Player Hand = Dealer HanDd, PUSH
-//Player Bet +- to Player Bank
-//If Player Bank = 0, Player Broke
-//If player Bank > 1, 
-
-//Game 2 Begins
